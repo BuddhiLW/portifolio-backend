613f92d22f90f66ad9d3068614022e6956c55895
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-03-20 14:53:32 -0300

    feat: connect API call to remote db

--- src/tecnologia/tecnologia.controller.ts
+++ src/tecnologia/tecnologia.controller.ts
@@ -1,25 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { Tecnologia } from '@core';
+import { TecnologiaProvider } from './tecnologia.provider';
 
 @Controller('tecnologias')
 export class TecnologiaController {
-  //   @Get()
-  //   async findAll() {
-  //     return ['Node.js', 'Express.js', 'Nest.js'];
-  //   }
-
+  constructor(private readonly repo: TecnologiaProvider) {}
   @Get()
   async findAll(): Promise<Tecnologia[]> {
-    return [
-      {
-        id: 1,
-        nome: 'Node.js',
-        descricao:
-          'Node.js ém uma plataforma de desenvolvimento de aplicativos baseada na linguagem JavaScript, que permite a execução de códigos JavaScript fora de um navegador web.',
-        imagem:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png',
-        destaque: true,
-      },
-    ];
+    return this.repo.findAll();
   }
 }
